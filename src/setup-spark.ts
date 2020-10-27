e2422f5bf673ef8d50133e8292951623db98a84c
Vincent Emonet <vincent.emonet@gmail.com>
2020-10-27 20:10:16 +0100

    small refactor

--- src/setup-spark.ts
+++ src/setup-spark.ts
@@ -34,23 +34,24 @@ try {
   
   const sparkHome = '/usr/local/spark';
   const py4jVersion = core.getInput('py4j-version');
+  
+  const SPARK_OPTS = `--driver-java-options=-Xms1024M --driver-java-options=-Xmx2048M --driver-java-options=-Dlog4j.logLevel=info`
   const PYTHONPATH = `${sparkHome}/python:${sparkHome}/python/lib/py4j-${py4jVersion}-src.zip`;
   const PYSPARK_PYTHON = 'python';
   // const PYSPARK_PYTHON = process.env.pythonLocation + '/bin/python';
 
-  // Set environment variable in the workflow
+  // Set environment variables in the workflow
   // See https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/
   exec(`echo "HADOOP_VERSION=${hadoopVersion}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
   exec(`echo "APACHE_SPARK_VERSION=${sparkVersion}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
   exec(`echo "SPARK_HOME=${sparkHome}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
-  exec(`echo "PYTHONPATH=${PYTHONPATH}" >> $GITHUB_ENV`, 
-        (err, stdout, stderr) => { });
-  exec(`echo "SPARK_OPTS=--driver-java-options=-Xms1024M --driver-java-options=-Xmx2048M --driver-java-options=-Dlog4j.logLevel=info" >> $GITHUB_ENV`, 
-        (err, stdout, stderr) => { });
-  exec(`echo "PATH=$PATH:${sparkHome}/bin" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
-
   exec(`echo "PYSPARK_PYTHON=${PYSPARK_PYTHON}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
   exec(`echo "PYSPARK_DRIVER_PYTHON=${PYSPARK_PYTHON}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
+  exec(`echo "PYTHONPATH=${PYTHONPATH}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
+  exec(`echo "SPARK_OPTS=${SPARK_OPTS}" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
+      
+  // Add Spark to path
+  exec(`echo "PATH=$PATH:${sparkHome}/bin" >> $GITHUB_ENV`, (err, stdout, stderr) => { });
 
   core.setOutput("spark-version", sparkVersion);
 } catch (error) {
