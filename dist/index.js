02e21ad411a54405231214167031d89442c351f8
Vincent Emonet <vincent.emonet@gmail.com>
2020-10-27 16:24:35 +0100

    fix install

--- dist/index.js
+++ dist/index.js
@@ -29,17 +29,7 @@ try {
     echo "${sparkChecksum} *spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" | sha512sum -c - && \
     sudo tar xzf "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" -C /usr/local --owner root --group root --no-same-owner &&
     rm "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" &&
-    ln -s "/usr/local/spark-${sparkVersion}-bin-hadoop${hadoopVersion}" /usr/local/spark`
-
-  // var exportCommand = `export &&
-  //   cd /tmp &&
-  //   find -type f -printf %T+\\t%p\\n | sort -n &&
-  //   wget -q $(wget -qO- https://www.apache.org/dyn/closer.lua/spark/spark-${sparkVersion}/spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz?as_json | python -c "import sys, json; content=json.load(sys.stdin); print(content['preferred']+content['path_info'])") &&
-  //   echo "${sparkChecksum} *spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" | sha512sum -c - &&
-  //   sudo tar xzf "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" -C /usr/local --owner root --group root --no-same-owner &&
-  //   rm "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" &&
-  //   ln -s "/usr/local/spark-${sparkVersion}-bin-hadoop${hadoopVersion}" /usr/local/spark`
-
+    sudo ln -s "/usr/local/spark-${sparkVersion}-bin-hadoop${hadoopVersion}" /usr/local/spark`
 
   exec(command, (err, stdout, stderr) => {
     console.log('stdout:');
@@ -56,6 +46,27 @@ try {
   process.env.PYTHONPATH = `${sparkHome}/python:${sparkHome}/python/lib/py4j-${py4jVersion}-src.zip`;
   process.env.SPARK_OPTS = `--driver-java-options=-Xms1024M --driver-java-options=-Xmx2048M --driver-java-options=-Dlog4j.logLevel=info`;
   process.env.PATH = process.env['PATH'] + `${sparkHome}/bin`;
+
+  var exportCommand = `export HADOOP_VERSION=${hadoopVersion} &&
+    export APACHE_SPARK_VERSION=${sparkVersion} &&
+    export HADOOP_VERSION=${hadoopVersion} &&
+    export SPARK_HOME=${sparkHome} &&
+    export PYTHONPATH=${sparkHome}/python:${sparkHome}/python/lib/py4j-${py4jVersion}-src.zip &&
+    export SPARK_OPTS=--driver-java-options=-Xms1024M --driver-java-options=-Xmx2048M --driver-java-options=-Dlog4j.logLevel=info &&
+    export PATH=$PATH:${sparkHome}/bin`
+
+    exec(exportCommand, (err, stdout, stderr) => {
+      console.log('exportCommand stdout:');
+      console.log(stdout);
+      console.log('exportCommand err:');
+      console.log(err);
+      console.log('exportCommand stderr:');
+      console.log(stderr);
+      console.log('exportCommand stderr:');
+      console.log(stderr);
+      console.log('process.env after the exportCommand:');
+      console.log(process.env);
+    });
 
   console.log('process.env after the install:');
   console.log(process.env);
