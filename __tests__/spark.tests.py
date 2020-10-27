469fe41f862a858be798d8ecd186644f31f07fe9
Vincent Emonet <vincent.emonet@gmail.com>
2020-10-27 18:58:44 +0100

    fix python pyspark version

---
+++ __tests__/spark.tests.py
@@ -0,0 +1,17 @@
+import sys,os
+
+## Example of test that could be done with python
+
+# os.environ["SPARK_HOME"] = '/opt/cloudera/parcels/CDH/lib/spark'
+# os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+# sys.path.append('/opt/cloudera/parcels/CDH/lib/spark/python')
+# sys.path.append('/opt/cloudera/parcels/CDH/lib/spark/python/lib/py4j-0.9-src.zip') 
+import pyspark
+from pyspark import SparkContext, SparkConf
+conf = SparkConf()
+conf.setMaster('yarn-client')
+conf.setAppName('test')
+sc = SparkContext(conf = conf)
+#sc = SparkContext.getOrCreate()
+rdd = sc.textFile("http://myfile.csv")
+rdd.take(10)
