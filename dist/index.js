629841a35e78e6ab843e55e1816f53f81c7c7bc4
Vincent Emonet <vincent.emonet@gmail.com>
2020-10-27 16:52:59 +0100

    fix permissions

--- dist/index.js
+++ dist/index.js
@@ -27,9 +27,10 @@ try {
     find -type f -printf %T+\\t%p\\n | sort -n &&
     wget -q $(wget -qO- https://www.apache.org/dyn/closer.lua/spark/spark-${sparkVersion}/spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz?as_json | python -c "import sys, json; content=json.load(sys.stdin); print(content['preferred']+content['path_info'])") &&
     echo "${sparkChecksum} *spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" | sha512sum -c - && \
-    sudo tar xzf "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" -C /usr/local --owner root --group root --no-same-owner &&
+    sudo tar xzf "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" -C /usr/local &&
     rm "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" &&
     sudo ln -s "/usr/local/spark-${sparkVersion}-bin-hadoop${hadoopVersion}" /usr/local/spark`
+  // sudo tar xzf "spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz" -C /usr/local --owner root --group root --no-same-owner &&
 
   exec(command, (err, stdout, stderr) => {
     console.log('stdout:');
