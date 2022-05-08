601c1c2c8cef3b0d32233d19b5c3c5ca42284d81
Vincent Emonet <vincent.emonet@gmail.com>
2022-05-08 11:49:38 +0200

    upgrade dependencies

--- __tests__/spark.tests.py
+++ __tests__/spark.tests.py
@@ -1,13 +1,14 @@
-import sys,os
-
-## Example of test that could be done with python
+import sys
 
 # os.environ["SPARK_HOME"] = '/opt/cloudera/parcels/CDH/lib/spark'
 # os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
 # sys.path.append('/opt/cloudera/parcels/CDH/lib/spark/python')
 # sys.path.append('/opt/cloudera/parcels/CDH/lib/spark/python/lib/py4j-0.9-src.zip') 
 import pyspark
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkConf, SparkContext
+
+## Example of test that could be done with python
+
 conf = SparkConf()
 conf.setMaster('yarn-client')
 conf.setAppName('test')
